024d2e5a2e011f5ce09cdd8f14d3fde99650280c
kykt35 <kykt35@gmail.com>
2018-11-28 20:30:35 +0900

    Add 7 actions to articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,40 @@
 class ArticlesController < ApplicationController
+
   def index
+    @articles = Article.includes(:user).order('created_at DESC')
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+
+    else
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params) if @article.user == current_user
+  end
+
+  def show
+  end
+
+  def destroy
+    @article.destroy if @article.user == current_user
+  end
+
+  private
+
+  def article_params
+    params.permit(:title.:content, :image).merge(user_id: current_user.id)
+  end
+
 end
