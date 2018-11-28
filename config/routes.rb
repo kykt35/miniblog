497b85439fcf35cdc26872fc022bf13f146cc76d
kykt35 <kykt35@gmail.com>
2018-11-29 00:32:52 +0900

    Set comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :users, only: [:edit, :update]
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:create]
+  end
 end
