3ec49854f5e8ea5469e97ed3cc48164066ac126c
kykt35 <kykt35@gmail.com>
2018-11-28 21:55:49 +0900

    Update articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,16 +19,20 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
     @article.update(article_params) if @article.user == current_user
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy if @article.user == current_user
   end
 
