14f7a70061cc0f92f8d00fabb61e0a26077893d9
kykt35 <kykt35@gmail.com>
2018-11-29 00:32:16 +0900

    Edit comments controller to add #creat and strong parameter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment=Comment.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.permit(:content, :article_id).merge(user_id: current_user.id)
+  end
 end
