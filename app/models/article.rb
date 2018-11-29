d4a33f55a96482fc56e5f182e68a44362d5a8c85
kykt35 <kykt35@gmail.com>
2018-11-29 11:50:58 +0900

    Add association option to destroy article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :content,presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
