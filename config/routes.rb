1bb0723c9551a0e871d61822f925588c2a18be92
kykt35 <kykt35@gmail.com>
2018-11-29 13:45:34 +0900

    Edit route to add user#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'articles#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :articles do
     resources :comments, only: [:create]
   end
