8f2411de46889d4609c2f8ac5814c428dc9e1df2
kykt35 <kykt35@gmail.com>
2018-11-29 11:50:29 +0900

    Edit strong parameter in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment=Comment.create(comment_params)
+
+    @comment=Comment.create(comment_params) if user_signed_in?
+
+    respond_to do |format|
+      format.html {
+        article = Article.find(@comment.article.id)
+        redirect_to article_path(article)
+         }
+      format.json
+    end
   end
 
   private
 
   def comment_params
-    params.permit(:content, :article_id).merge(user_id: current_user.id)
+    params.require(:comment).permit(:content).merge(article_id: params[:article_id],user_id: current_user.id)
   end
 end
